3aaab2ce7fc1930db993029f6eeae0de812df516
PARK <PARK@W11USER>
2024-08-09 11:07:43 +0900

    ..

---
+++ Ch03Form/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Ch03Form
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "점수:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(119, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "학점:";
+            // 
+            // tbInput
+            // 
+            this.tbInput.Location = new System.Drawing.Point(195, 51);
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(100, 21);
+            this.tbInput.TabIndex = 2;
+            // 
+            // tbResult
+            // 
+            this.tbResult.Location = new System.Drawing.Point(195, 143);
+            this.tbResult.Name = "tbResult";
+            this.tbResult.Size = new System.Drawing.Size(100, 21);
+            this.tbResult.TabIndex = 3;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(123, 95);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(172, 23);
+            this.btnCalc.TabIndex = 4;
+            this.btnCalc.Text = "학점확인";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 238);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbResult);
+            this.Controls.Add(this.tbInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ Ch03Form/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch03Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("버튼 클릭 이벤트!!!");
+
+            int score = Convert.ToInt32(tbInput.Text);
+            String result = "";
+
+            if (score >= 95) {
+                result = "A+";
+            } else if (score >= 90) {
+                result = "A";
+            } else if (score >= 85) {
+                result = "B+";
+            } else if (score >= 80) {
+                result = "B";
+            } else if (score >= 75) {
+                result = "C+";
+            } else if (score >= 70) {
+                result = "C";    
+            } else if (score >= 65) {
+                result = "D+";
+            } else if (score >= 60) {
+                result = "D";
+            } else {
+                result = "F";
+            }
+
+            tbResult.Text = result;
+
+
+
+
+
+        }
+    }
+}
