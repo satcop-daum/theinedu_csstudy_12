ac7276955a26e0ada90d4b00124c8975c5567335
PARK <PARK@W11USER>
2024-08-14 15:49:14 +0900

    4일차 소스 파일

---
+++ Ch04Form/Form2.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Ch04Form
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.tbOuput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbInput.Location = new System.Drawing.Point(12, 12);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbInput.Size = new System.Drawing.Size(1059, 281);
+            this.tbInput.TabIndex = 0;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("굴림", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnRun.Location = new System.Drawing.Point(12, 299);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(113, 48);
+            this.btnRun.TabIndex = 1;
+            this.btnRun.Text = "실행";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // tbOuput
+            // 
+            this.tbOuput.Font = new System.Drawing.Font("굴림", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbOuput.Location = new System.Drawing.Point(12, 353);
+            this.tbOuput.Multiline = true;
+            this.tbOuput.Name = "tbOuput";
+            this.tbOuput.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbOuput.Size = new System.Drawing.Size(1059, 315);
+            this.tbOuput.TabIndex = 2;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1083, 680);
+            this.Controls.Add(this.tbOuput);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.tbInput);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.TextBox tbOuput;
+    }
+}
---
+++ Ch04Form/Form2.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch04Form
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        bool ExistsYn(string[] texts, string text)
+        {
+            bool result = false;
+
+            for(int i = 0; i < texts.Length; i++)
+            {
+                if (text.Length > 0 && texts[i] == text)
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+
+        /*
+        - 쩜, 콤마를 공백으로 변환
+        - 대소문는 구분하지 않음
+        - 공백으로 split
+        - 양쪽끝공백제거한 문자열의 길이가 0보다 크면
+            => 단어
+        */
+        string TextProcessing(string text)
+        {
+            text = text.Replace(".", " ");
+            text = text.Replace(",", " ");
+            text = text.Replace(";", " ");
+            text = text.Replace("\r\n", " ");
+            text = text.ToLower();
+
+            string[] resultTexts = new string[1];
+            int[] resultTextsCount = new int[1];
+            int index = 0;
+
+            string[] allTexts = text.Split(' ');
+            for (int i = 0; i < allTexts.Length; i++)
+            {
+                string curText = allTexts[i].Trim();
+                if (!string.IsNullOrEmpty(curText))
+                {
+                    bool existsYn = ExistsYn(resultTexts, curText);
+
+                    if (!existsYn)
+                    {
+                        resultTexts[index] = curText;
+                        index++;
+                        Array.Resize(ref resultTexts, resultTexts.Length + 1);
+                        Array.Resize(ref resultTextsCount, resultTextsCount.Length + 1);
+                    }
+                }
+            }
+
+          
+            for(int i = 0; i < resultTexts.Length;i++)
+            {
+                string resultText = resultTexts[i];
+
+                for (int j = 0; j < allTexts.Length; j++)
+                {
+                    string allText = allTexts[j];
+
+                    if (resultText == allText)
+                    {
+                        resultTextsCount[i] += 1;
+                    }
+                }
+            }
+
+            string result = "";
+            for (int i = 0; i < resultTexts.Length; i++)
+            {
+                result += string.Format("{0}:\t\t:{1}\r\n", resultTexts[i], resultTextsCount[i]);
+            }
+
+            return result;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string text = tbInput.Text;
+
+            string result = TextProcessing(text);
+
+            tbOuput.Text = result;
+        }
+    }
+}
