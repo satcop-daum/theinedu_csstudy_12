29d08359a872e95a28d7306656a3016be22363e6
박규태 <satcop@naver.com>
2024-08-20 11:43:05 +0900

    5일차 실습 소스

---
+++ Ch05Form/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Ch05Form
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.tmrMain = new System.Windows.Forms.Timer(this.components);
+            this.nudAlarmHour = new System.Windows.Forms.NumericUpDown();
+            this.nudAlarmMinute = new System.Windows.Forms.NumericUpDown();
+            this.cbAlarmYn = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlarmHour)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlarmMinute)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK.Location = new System.Drawing.Point(20, 252);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(177, 60);
+            this.btnOK.TabIndex = 0;
+            this.btnOK.Text = "버튼 클릭";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // lblTime
+            // 
+            this.lblTime.BackColor = System.Drawing.Color.Black;
+            this.lblTime.Font = new System.Drawing.Font("Lucida Console", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.lblTime.Location = new System.Drawing.Point(12, 9);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(510, 70);
+            this.lblTime.TabIndex = 1;
+            this.lblTime.Text = "label1";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrMain
+            // 
+            this.tmrMain.Enabled = true;
+            this.tmrMain.Tick += new System.EventHandler(this.tmrMain_Tick);
+            // 
+            // nudAlarmHour
+            // 
+            this.nudAlarmHour.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudAlarmHour.Location = new System.Drawing.Point(316, 148);
+            this.nudAlarmHour.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.nudAlarmHour.Name = "nudAlarmHour";
+            this.nudAlarmHour.Size = new System.Drawing.Size(80, 46);
+            this.nudAlarmHour.TabIndex = 2;
+            // 
+            // nudAlarmMinute
+            // 
+            this.nudAlarmMinute.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudAlarmMinute.Location = new System.Drawing.Point(411, 148);
+            this.nudAlarmMinute.Maximum = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.nudAlarmMinute.Name = "nudAlarmMinute";
+            this.nudAlarmMinute.Size = new System.Drawing.Size(70, 46);
+            this.nudAlarmMinute.TabIndex = 3;
+            this.nudAlarmMinute.ValueChanged += new System.EventHandler(this.nudAlarmMinute_ValueChanged);
+            // 
+            // cbAlarmYn
+            // 
+            this.cbAlarmYn.AutoSize = true;
+            this.cbAlarmYn.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbAlarmYn.Location = new System.Drawing.Point(279, 163);
+            this.cbAlarmYn.Name = "cbAlarmYn";
+            this.cbAlarmYn.Size = new System.Drawing.Size(15, 14);
+            this.cbAlarmYn.TabIndex = 4;
+            this.cbAlarmYn.UseVisualStyleBackColor = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 324);
+            this.Controls.Add(this.cbAlarmYn);
+            this.Controls.Add(this.nudAlarmMinute);
+            this.Controls.Add(this.nudAlarmHour);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.btnOK);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "윈폼 프로그래밍";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.SizeChanged += new System.EventHandler(this.frmMain_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlarmHour)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAlarmMinute)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer tmrMain;
+        private System.Windows.Forms.NumericUpDown nudAlarmHour;
+        private System.Windows.Forms.NumericUpDown nudAlarmMinute;
+        private System.Windows.Forms.CheckBox cbAlarmYn;
+    }
+}
+
---
+++ Ch05Form/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ch05Form
+{
+    public partial class frmMain : Form
+    {
+        int value1 = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show("버튼 클릭!!!!");
+
+        }
+
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            //ResizeCotnrols();
+
+        }
+
+        private void frmMain_SizeChanged(object sender, EventArgs e)
+        {
+            //ResizeCotnrols();
+        }
+
+        void ResizeCotnrols()
+        {
+            //(폼의Width - 버튼의Width) / 2 => 버튼의 x좌표
+            //(폼의Height - 버튼의Height) / 2 => 버튼의 y좌표
+
+            int buttonWidth = ClientSize.Width * 80 / 100;
+            int buttonHeight = ClientSize.Height * 80 / 100;
+            btnOK.Size = new Size(buttonWidth, buttonHeight);
+
+            int x = (ClientSize.Width - btnOK.Width) / 2;
+            int y = (ClientSize.Height - btnOK.Height) / 2;
+            btnOK.Location = new Point(x, y);
+        }
+
+        private void tmrMain_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            String time = "";
+
+            if (now.Millisecond < 500)
+            {
+                time = now.ToString("HH:mm:ss");
+            }
+            else
+            {
+                time = now.ToString("HH:mm ss");
+            }
+
+            if (now.Hour < 12)
+            {
+                time = "AM " + time;
+            } else
+            {
+                time = "PM " + time;
+            }
+
+            lblTime.Text = time;
+
+
+            //알람확인
+            if (cbAlarmYn.Checked)
+            {
+
+                //현재시간 알람을 설정한 시간의 차이 => 1분미만일때 체크
+                int gap = (int)(nudAlarmHour.Value * 60 + nudAlarmMinute.Value) - (now.Hour * 60 + now.Minute);
+                
+                if (gap <= 1)
+                {
+                    if (now.Millisecond < 500)
+                    {
+                        lblTime.BackColor = Color.Red;
+                    }
+                    else
+                    {
+                        lblTime.BackColor = Color.Black;
+                    }
+
+                }
+
+
+
+                if (now.Hour == nudAlarmHour.Value 
+                    && now.Minute == nudAlarmMinute.Value)
+                {
+                    cbAlarmYn.Checked = false;
+                    MessageBox.Show("알람시간이되었습니다.");
+                }
+
+            }
+
+        }
+
+        private void nudAlarmMinute_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
